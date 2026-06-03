9bb82c21762a1c5fd13c08b7d47d20cb8baad57b
sanskar-tutorials <artistic.ransh1098@gmail.com>
2026-06-03 13:54:43 +0530

    ArrayList Basics

---
+++ Array/BuildingArray2.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.Scanner;
+public class BuildingArray2 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    sc.close();
+    buildings(arr, n);
+  }
+
+  public static void buildings(int[] arr, int n) {
+    // your code here
+        // Find maximum height
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        // Print pattern row by row
+        for (int row = max; row >= 1; row--) {
+
+            for (int col = 0; col < n; col++) {
+
+                if (arr[col] >= row) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+  }
+}
+}
